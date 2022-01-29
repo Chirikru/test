68e97ed73c377d501ec40dac6c7a1d72b6b551a9
Chirikru <98220560+Chirikru@users.noreply.github.com>
2022-01-30 00:16:16 +0300

    Update alertmanager-config.sh

--- util/alertmanager-config.sh
+++ util/alertmanager-config.sh
@@ -2,4 +2,4 @@
 #!/bin/sh
 script=$0
 
-sed -i "s|https://ngrok.io|$1|" $(dirname "$script")/../alertmanager/config.yml
+sed -i "s|https://api.telegram.org/bot5209561422:AAEEPNrU6AgO2IxBTykaehhonAQ4E6M37NY/sendmessage?chat_id=539686084&text=%22Alerts500X%22|$1|" $(dirname "$script")/../alertmanager/config.yml
