9acb1370e134acb2ea8ddb0d5da44a457152dcf2
Chirikru <98220560+Chirikru@users.noreply.github.com>
2022-01-30 00:49:12 +0300

    Delete alertmanager-config.sh

--- util/alertmanager-config.sh
+++
@@ -1,5 +0,0 @@
-
-#!/bin/sh
-script=$0
-
-sed -i "s|https://api.telegram.org/bot5209561422:AAEEPNrU6AgO2IxBTykaehhonAQ4E6M37NY/sendmessage?chat_id=539686084&text=%22Alerts500X%22|$1|" $(dirname "$script")/../alertmanager/config.yml
